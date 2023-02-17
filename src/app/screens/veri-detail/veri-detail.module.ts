f7a5bfd2d6d6abba36dbb9d378cd5cd9a8a160c5
jchango <jchango@icesa.ec>
2023-02-17 10:06:34 -0500

    mensaje

--- src/app/screens/veri-detail/veri-detail.module.ts
+++ src/app/screens/veri-detail/veri-detail.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule,ReactiveFormsModule} from '@angular/forms';
-
+import { Router, NavigationExtras } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
@@ -32,3 +32,4 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class VeriDetailPageModule {}
+

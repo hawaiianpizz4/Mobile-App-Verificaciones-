55c2a0426b3e80399094e9b4c52536399da53433
jchango <jchango@icesa.ec>
2023-02-24 14:33:50 -0500

    refactorización Detalle

--- src/app/screens/veri-detail/veri-detail.page.ts
+++ src/app/screens/veri-detail/veri-detail.page.ts
@@ -1,6 +1,6 @@
 import mapboxgl from 'mapbox-gl/';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, Input } from '@angular/core';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
@@ -33,12 +33,18 @@ import { Router } from '@angular/router';
 
 })
 export class VeriDetailPage implements OnInit {
+  // @Input() getdata;
+
   getdata = {};
   map;
   infoPoss = [];
   status: boolean;
   address: string;
   public photosPlanilla: UserPhoto[];
+
+dataForm: FormGroup;
+
+
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -53,50 +59,63 @@ export class VeriDetailPage implements OnInit {
     private route: ActivatedRoute,
     private modalCtrl: ModalController
   ) {
-    this.getdata = {
-      codigo: ' IDCMS',
-      nombre_gestor: ' Juan Perez',
-      fecha_actual: ' 15/02/2023',
-      nombre_tienda: ' Orve Condado',
-      nombre_cliente: ' Maria Sanchez',
-      numero_cedula: ' 1717830705',
-      direccion_cliente:'Quicentro Sur',
-      //vivienda
-      tipo_vivienda: 'Propia',
-      persona_verifica: 'Maria Sanchez',
-      residencia_minima: 'Si',
-      localTerreno_propio:'Si',
-      localTerreno_arrendado:'Si',
-      //servicios
-      planilla_servicios: 'Si',
-      imagen_planilla: 'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/PLANILLA - 3123123131 - 08944780-6882-94EA-46B9-56107F90B6A5 - 1.png',
-      seguridad_puertas: 'si',
-      muebleria_basica: 'si',
-      material_casa: 'Bloque',
-      periodicidad_actividades: 'DIARIO',
-      //vecinos
-      vecino_confirm: 'Si',
-      vecino_nombre: 'Danilo Carrera',
-      vecino_celular: '0969838598',
-
-      //imágenes
-      imagen_estabilidad: 'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/ESTABILIDAD - 3123123131 - 5946861D-8A01-3FEC-0290-02C6BF4CFA2E - 1.png',
-      imagen_proveedores: 'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/FACTURAS_PROV - 3123123131 - 82E2AC33-3911-7462-E5E5-F4D51B53B5A9 - 1.png',
-      imagen_exteriorNegocio: 'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/NEGO_EXTERIOR - 3123123131 - F19524EC-5FC5-A4DB-2176-3D30352E704F - 1.png',
-      imagen_interiorNegocio: 'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/NEGO_INTERIOR - 3123123131 - D920EE3D-A05B-72D0-D71B-D946711C514E - 1.png',
-      imagen_clienteInteriorNegocio:'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/CLIENTE_NEGO_INTERIOR - 3123123131 - 8A4AEBCC-990A-2E56-5F12-A323B9D5C6DB - 1.png',
-      imagen_clienteExteriorNegocio:'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/CLIENTE_NEGO_EXTERIOR - 3123123131 - 82A6ABA1-54CA-3BA6-48AD-A7F568158117 - 1.png',
-      imagen_tituloPropiedad:'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/TITULO_PROPIEDAD - 3123123131 - 273D9BED-8A06-6A36-0FD8-EF59DDD951B8 - 1.png',
-      imagen_impuestoPredial: 'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/PREDIAL - 3123123131 - 5A2DBC1E-54E9-E2A8-0385-858258C49433 - 1.png',
-      imagen_respaldoIngresos: 'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/RESPALDO - 3123123131 - 8BF22D93-39EC-FF71-25B2-7AA65B9E80C9 - 1.png',
-      imagen_certificadoLaboral: 'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/CERTIFICADO - 3123123131 - 53624914-2915-5DB4-3C50-4F798E2EEBFF - 1.png',
-      imagen_interiorDomicilio: 'http://200.7.249.21:90/VerificacionesFisicas/APP_Cobranzas_Fotos/Verificacion/JUAN PEREZ/INTERIOR_DOMICILIO - 3123123131 - 199AA52D-9CD8-F1FC-6072-61771657E256 - 1.png',
 
 
+  this.getdata =   {
+      "nombreGestor": "mario Cardenas",
+      "fechaverificacion": "2023-02-24 10:37:18",
+      "vf_nombre_tienda": "vf_nombre_tienda",
+      "nombreCliente": "nombreCliente",
+      "cedulaCliente": "cedulaCliente",
+      //
+      "tipoVivienda": "tipoVivienda",
+      "personaQuienRealizaLaVerificación": "personaQuienRealizaLaVerificación",
+      "residenciaMinimaTresMeses": "residenciaMinimaTresMeses",
+      "localTerrenoPropio": "localTerrenoPropio",
+      "localTerrenoArrendado": "localTerrenoArrendado",
+      "planillaServicioBasico": "planillaServicioBasico",
+      "planillaServicioBasicoImagen": "planillaServicioBasicoImagen",
+      "seguridadPuertasVentanas": "seguridadPuertasVentanas",
+      "muebleriaBasica": "muebleriaBasica",
+      "materialCasa": "materialCasa",
+      "periodicidadActividadesLaborales": "periodicidadActividadesLaborales",
+      "confirmacionInfoVecinos": "confirmacionInfoVecinos",
+      "nombreInfoVecino": "nombreInfoVecino",
+      "celularInfoVecino": "celularInfoVecino",
+
+
+  };
+
+  this.dataForm = new FormGroup
+  ({
+    nombre_gestor: new FormControl(this.getdata["nombreGestor"], []),
+    fechaverificacion: new FormControl(this.getdata["fechaverificacion"]),
+    vf_nombre_tienda: new FormControl(this.getdata["vf_nombre_tienda"]),
+    nombreCliente: new FormControl(this.getdata["nombreCliente"]),
+    cedulaCliente: new FormControl(this.getdata["cedulaCliente"]),
+    tipoVivienda: new FormControl(this.getdata["tipoVivienda"]),
+    personaQuienRealizaLaVerificación: new FormControl(this.getdata["personaQuienRealizaLaVerificación"]),
+    residenciaMinimaTresMeses: new FormControl(this.getdata["residenciaMinimaTresMeses"]),
+    localTerrenoPropio: new FormControl(this.getdata["localTerrenoPropio"]),
+    localTerrenoArrendado: new FormControl(this.getdata["localTerrenoArrendado"]),
+    planillaServicioBasico: new FormControl(this.getdata["planillaServicioBasico"]),
+    planillaServicioBasicoImagen: new FormControl(this.getdata["planillaServicioBasicoImagen"]),
+    seguridadPuertasVentanas: new FormControl(this.getdata["seguridadPuertasVentanas"]),
+    muebleriaBasica: new FormControl(this.getdata["muebleriaBasica"]),
+    materialCasa: new FormControl(this.getdata["materialCasa"]),
+    periodicidadActividadesLaborales: new FormControl(this.getdata["periodicidadActividadesLaborales"]),
+    confirmacionInfoVecinos: new FormControl(this.getdata["confirmacionInfoVecinos"]),
+    nombreInfoVecino: new FormControl(this.getdata["nombreInfoVecino"]),
+    celularInfoVecino: new FormControl(this.getdata["celularInfoVecino"]),
+
+
+  })
 
 
 
-    };
+
+
+
   }
 
   ngOnInit() {
@@ -107,6 +126,44 @@ export class VeriDetailPage implements OnInit {
 
   }
 
+cargaDatosBase(){
+
+
+};
+
+/*
+console.dir(postData);
+
+if (postData.cliente_cedula && postData.cliente_cedula != undefined) {
+  if (this.networkStatus) {
+    const url = `${environment.apiUrl}refinanciamiento.php?opcion=postDatosRefi`;
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+
+    try {
+      await this._http.post(url, JSON.stringify(postData), httpOptions).toPromise();
+      this.isServiceCallInProgress.dismiss();
+      this.redirect();
+      setTimeout(() => {
+        presentToast('Registro Enviado', 'checkmark-outline', 'success');
+        this.redirect();
+      }, 1000);
+    } catch (error) {
+      this.isServiceCallInProgress.dismiss();
+      setTimeout(() => {
+        presentToast('Error al enviar registro', 'checkmark-outline', 'success');
+        this.redirect();
+      }, 3000);
+      console.log(error);
+    }
+
+
+
+*/
 
 
   initMap() {
